5cfc467ebf936900d7ecf520471abf6ac5496fa3
h581876 <581876@stud.hvl.no>
2025-08-25 14:32:59 +0200

    implementert 1b

---
+++ src/oppgave1/Oppg1b.java
@@ -0,0 +1,34 @@
+package oppgave1;
+
+import java.util.function.BiFunction;
+
+public class Oppg1b {
+	
+	public static int beregn(int a, int b, BiFunction<Integer, Integer, Integer> funksjon) {
+		return funksjon.apply(a, b);
+		}
+
+	public static void main(String[] args) {
+		
+		
+		BiFunction<Integer, Integer, Integer> summerFunksjon = (a, b) -> a + b;
+		
+		BiFunction<Integer, Integer, Integer> størsteFunksjon = (a, b) -> Math.max(a, b);
+		
+		BiFunction<Integer, Integer, Integer> avstandFunksjon = (a, b) -> Math.abs(a - b);
+		
+		int sum = beregn(12, 13, summerFunksjon);
+		
+		System.out.println(sum);
+		
+		int største = beregn(-5, 3, størsteFunksjon);
+		
+		System.out.println(største);
+		
+		int avstand = beregn(54, 45, avstandFunksjon);
+		
+		System.out.println(avstand);
+		
+	}
+
+}
